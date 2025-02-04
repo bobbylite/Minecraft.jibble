180e42b2a5eb415ca826729f9790f641c3734710
Bobby <bobby.luisi@gmail.com>
2025-02-04 02:25:54 -0500

    Adding python support

--- minecraft-jibble/src/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("favicon.ico"); // This must be in your project root
-    res.compile().expect("Failed to set application icon");
-}
